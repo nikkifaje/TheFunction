61dee309896b1bb82999dc793517bbbf06c862e2
Justin Hanley <jhanley412@gmail.com>
2015-11-23 14:39:52 -0800

    Add parameter lookup for lens identification using regular expression

--- cubi_function.py
+++ cubi_function.py
@@ -1,3 +1,4 @@
+import re
 
 
 def query_function(lens = 'NULL', output = 'NULL'):
@@ -6,8 +7,10 @@ def query_function(lens = 'NULL', output = 'NULL'):
 
     try:
         print('Begin')
-        file_location = r'C:\Python34\VEnvs\MSSQL\pymssql\Function\TheFunction.sql'
-        file = open(file_location).readlines()
+        file_location = r'C:\Python34\VirtualEnvs\CUBITest\TheFunction\ACUFunction.sql'
+        read_file_string = open(file_location).read()
+       
+        
         #print(file)
         
         sql_function_head = '''
@@ -40,8 +43,13 @@ def query_function(lens = 'NULL', output = 'NULL'):
             SET
         '''
 
+        strip_items_dictionary = {'(':'', ')':'', ',':''}
+
+        
+        #Capture all definitions that need to be declared in sql function
+        read_file_list = read_file_string.split('\n')
         variables_dictionary = {}
-        for line in file:
+        for line in read_file_list:
             word_list = line.split()
             strip_items_dictionary = {'(':'', ')':'', ',':''}
             for word in word_list:
@@ -54,6 +62,23 @@ def query_function(lens = 'NULL', output = 'NULL'):
                             continue
                     variables_dictionary[word_alter] = None
         print(variables_dictionary)
+
+
+        #Build output sql query for relevant lens paramters to optimize final query
+        #execution. Read through eac set of LEFT OUTER JOINS and build requested
+        #select statement accoring to string format parameter
+        sql_output_list = read_file_string.split(':BEGINOUTPUT')
+        sql_left_join_list = sql_output_list[1].split('LEFT OUTER JOIN')
+        for left_join_group in sql_left_join_list[0:5]:
+            #print(repr(left_join_group))
+            words_list = re.split(r' |\n*', left_join_group)
+            #print(repr(word))
+            #for word in left_join_group.re.split(r'\s*', word):
+            for word in words_list:
+                if "'%:" in word:
+                    print(repr(word))
+            #print('LEFT OUTER JOIN '+join_group)
+            
         
         if output.upper() == 'GETRESULTS':
             pass
